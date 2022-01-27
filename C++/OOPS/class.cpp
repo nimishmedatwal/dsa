73ca39b7a4de34a6075579b0b9983074ffd3aec0
Nimish Medatwal <medatwalnimish@gmail.com>
2022-01-27 18:04:17 +0530

    Class.cpp added

---
+++ C++/OOPS/class.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class student{
+    public:
+    string name;
+    int age;
+    bool gender;
+    void printinfo(){
+        cout<<"Name : "<<name<<endl;
+        cout<<"Age :" <<age<<endl;
+        cout<<"Gender : "<<gender<<endl;
+    }
+};
+int main(){
+
+    student arr[3];
+    for (int i=0;i<3;i++){
+        cin>>arr[i].name;
+        cin>>arr[i].age;
+        cin>>arr[i].gender;
+    }
+}
