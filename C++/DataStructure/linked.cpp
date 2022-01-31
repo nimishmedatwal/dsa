52bcaa6e5dcedc000b3ece6d7447ea0c6e635d45
Nimish Medatwal <medatwalnimish@gmail.com>
2022-01-31 17:48:03 +0530

    Completed linked.cpp

--- C++/DataStructure/linked.cpp
+++ C++/DataStructure/linked.cpp
@@ -11,12 +11,33 @@ class node{
     }
 };
 void insertAtTail(node* &head, int val){
+
     node* n=new node(val);
+    if (head==NULL){
+        head=n;
+        return ;
+    }
     node* temp=head;
     while (temp->next!=NULL){
-        
-    }
+        temp=temp->next;
+        }
+    temp->next=n;
+
 
 }
-
-
+void display(node* &head){
+    node* temp =head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+    cout<<endl;
+}
+int main(){
+    node* head=NULL;
+    insertAtTail(head,1);
+    insertAtTail(head,2);
+    insertAtTail(head,3);
+    display(head);
+    return 0; 
+}
