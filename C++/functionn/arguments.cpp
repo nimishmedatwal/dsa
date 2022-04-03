0ada103303ed54a472b67c21429aac3fe62e26c4
Nimish Medatwal <medatwalnimish@gmail.com>
2022-04-03 16:41:56 +0530

    added argument

---
+++ C++/functionn/arguments.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class A
+{
+    int i, j;
+
+public:
+    A(int a = -99, int b = -88)
+    {
+        i = a;
+        j = b;
+    }
+    void show() { cout << "i=" << i << " j = " << j << endl; }
+};
+int main()
+{
+    A a, b(2), c(1, 2);
+    a.show();
+    b.show();
+    c.show();
+}
