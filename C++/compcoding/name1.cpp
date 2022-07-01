d099ea62fc403560db994688d59f047e54790f74
Nimish Medatwal <medatwalnimish@gmail.com>
2022-07-01 22:57:56 +0530

    added name1.cpp

---
+++ C++/compcoding/name1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while (T--){
+        string a,b;
+        cin>>a>>b;
+        a=a+b;
+
+        int n;
+        cin>>n;
+        string s;
+        string temp;
+        for(int i=0;i<n;i++){
+            cin>>temp;
+            s+=temp;
+        }
+        int counter;
+        for (int i=0 ;i<a.length();i++){
+            counter=0;
+            for (int j=0;j<s.length();j++){
+                if(a[i]==s[j]){
+                    counter++;
+                    break;
+                }
+            }
+            if(counter==0){
+                cout<<"NO"<<endl;
+                break;
+            }
+
+        }
+        if(counter==1)
+            cout<<"YES"<<endl;
+    }
+}
