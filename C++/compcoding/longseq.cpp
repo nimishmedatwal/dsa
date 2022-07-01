9c188878d39b16be22aa2bf2f6ef03e25c38897c
Nimish Medatwal <medatwalnimish@gmail.com>
2022-07-01 16:22:53 +0530

    added longseq.cpp

--- C++/compcoding/longseq.cpp
+++ C++/compcoding/longseq.cpp
@@ -1,19 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
+int main()
+{
     int T;
     cin >> T;
-    while (T--){
+    while (T--)
+    {
         int n;
         cin >> n;
-        string s=to_string(n);  
-        int count0=0;
-        int count1=0;
-        for (int i=0;i<s.length();i++){
-            if (s[i]=='0') count0++;
-            else count1++;
+        string s = to_string(n);
+        int count0 = 0;
+        int count1 = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s[i] == '0')
+            {
+                count0++;
+            }
+            else
+                count1++;
         }
-        if (count0==1 || count1==1) cout << "YES" << endl;
-        else cout << "NO" << endl;
-     }
+        cout << count0 << " " << count1 << endl;
+        if (count0 == 1 || count1 == 1)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
 }
