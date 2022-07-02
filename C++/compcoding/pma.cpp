e3f8682d7b6f21673eae8e60eb358cc8491a39fb
Nimish Medatwal <medatwalnimish@gmail.com>
2022-07-02 23:50:13 +0530

    added remelem.cpp

---
+++ C++/compcoding/pma.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int n;
+        cin>>n;
+        int a[n];
+        int sum=0;
+        int ptr;
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+            if (i % 2 == 0)
+            {
+                ptr=abs(a[i]);
+            }
+            else{
+                ptr=-abs(a[i]);
+            }
+            sum+=ptr;
+
+        }
+        cout<<sum<<endl;
+        
+    }
+}
---
+++ C++/compcoding/remelem.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int T;
+    cin>>T;
+    while (T--){
+        int n,k;
+        cin>>n>>k;
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            cin>>a[i];
+
+        }
+        for (int i = 0; i < n; i++)
+        {
+
+        }
+
+    }
+}
