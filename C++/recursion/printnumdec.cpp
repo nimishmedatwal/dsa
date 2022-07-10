f9a6878aaa95f4bc6967af0faacc3230d386c0b5
Nimish Medatwal <medatwalnimish@gmail.com>
2022-07-10 11:52:59 +0530

    added decprint.cpp

--- C++/recursion/printnumdec.cpp
+++ C++/recursion/printnumdec.cpp
@@ -1,16 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-void printnum(int n){
-    if (n==0){
-        return;   
+void dec(int n)
+{
+    if (n == 0)
+    {
+        return;
     }
-    cout<<n<<" ";
-    printnum(n-1);
-    
+    cout << n << " ";
+    dec(n - 1);
 }
-int main(){
-    
-    printnum(7);
+int inc(int n){
+    if (n==0){
+        return 0;
+    }
+    int prev=inc(n-1);
+    cout<<n<< " ";
+    return n;
+}
+int main()
+{
+
+    inc(7);
     return 0;
 }
 //  6
