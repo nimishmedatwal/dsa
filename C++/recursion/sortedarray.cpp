db752491acfa71f434aad0beb3ac0084e2222ca9
Nimish Medatwal <medatwalnimish@gmail.com>
2022-07-10 11:16:44 +0530

    added sortedarray.cpp

---
+++ C++/recursion/sortedarray.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+bool sort(int arr[],int n){
+    if (n==2 && arr[0]<arr[1]){
+        return true;
+    }
+    int rest= sort(arr+1,n-1);
+    if (arr[0]<arr[1] && rest){
+        return true;
+    }
+    else return false;
+    
+
+}
+int main (){
+    int arr[]={1,1,4,2,3,4,5};
+    cout<<sort(arr,5);
+    return 0;
+}
+// 1 2 3 4 5 6 7 
