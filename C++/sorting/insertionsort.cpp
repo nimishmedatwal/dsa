66e64528e2d95475a76385532b9818dc28800c48
Nimish Medatwal <medatwalnimish@gmail.com>
2022-11-17 01:09:18 +0530

    added shellSort

---
+++ C++/sorting/insertionsort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void insertionSort(int* arr, int n){
+    for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
+
+        }
+    }
+}
+int main(){
+    int n=8;
+    int arr[n]={3,1,8,2,4,1,3,7};
+    insertionSort(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
